2e01b1a8cd9bdd5095928987a46897da26e222cb
eIbrahim67 <ibrahim.mohamed.ibrahim.t@gmail.com>
2024-07-26 01:13:59 +0300

    Add project files.

---
+++ scroll using your voice/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ScrollUsingYourVoice
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scrollUp = new System.Windows.Forms.Button();
+            this.scrollDown = new System.Windows.Forms.Button();
+            this.comboBoxWindows = new System.Windows.Forms.ComboBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // scrollUp
+            // 
+            this.scrollUp.Location = new System.Drawing.Point(495, 12);
+            this.scrollUp.Name = "scrollUp";
+            this.scrollUp.Size = new System.Drawing.Size(293, 137);
+            this.scrollUp.TabIndex = 0;
+            this.scrollUp.Text = "Scroll Up";
+            this.scrollUp.UseVisualStyleBackColor = true;
+            this.scrollUp.Click += new System.EventHandler(this.buttonScrollUp_Click);
+            // 
+            // scrollDown
+            // 
+            this.scrollDown.Location = new System.Drawing.Point(495, 301);
+            this.scrollDown.Name = "scrollDown";
+            this.scrollDown.Size = new System.Drawing.Size(293, 137);
+            this.scrollDown.TabIndex = 1;
+            this.scrollDown.Text = "Scroll Down";
+            this.scrollDown.UseVisualStyleBackColor = true;
+            this.scrollDown.Click += new System.EventHandler(this.buttonScrollDown_Click);
+            // 
+            // comboBoxWindows
+            // 
+            this.comboBoxWindows.FormattingEnabled = true;
+            this.comboBoxWindows.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.comboBoxWindows.IntegralHeight = false;
+            this.comboBoxWindows.Location = new System.Drawing.Point(62, 42);
+            this.comboBoxWindows.Name = "comboBoxWindows";
+            this.comboBoxWindows.Size = new System.Drawing.Size(375, 24);
+            this.comboBoxWindows.TabIndex = 2;
+            this.comboBoxWindows.SelectedIndexChanged += new System.EventHandler(this.comboBoxWindows_SelectedIndexChanged);
+            this.comboBoxWindows.Click += new System.EventHandler(this.comboBoxWindows_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.Location = new System.Drawing.Point(87, 167);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(207, 170);
+            this.labelOutput.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 158);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.comboBoxWindows);
+            this.Controls.Add(this.scrollDown);
+            this.Controls.Add(this.scrollUp);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button scrollUp;
+        private System.Windows.Forms.Button scrollDown;
+        private System.Windows.Forms.ComboBox comboBoxWindows;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ scroll using your voice/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Speech.Recognition;
+using System.Windows.Forms;
+using System.Diagnostics;
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+namespace ScrollUsingYourVoice
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        private SpeechRecognitionEngine recognizer;
+        private IntPtr selectedWindowHandle;
+        private bool isListening = false;
+
+        private const int KEYEVENTF_KEYUP = 0x0002;
+        private const byte VK_UP = 0x26;
+        private const byte VK_DOWN = 0x28;
+        Process chromeProcess;
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeSpeechRecognition();
+            RefreshWindowList();
+        }
+
+        private void InitializeSpeechRecognition()
+        {
+            recognizer = new SpeechRecognitionEngine();
+            recognizer.SetInputToDefaultAudioDevice();
+
+            var commands = new Choices(new[] { "Scroll up", "Scroll down",
+                "close chrome", "up audio", "down audio", 
+                "mute audio", "close the app" });
+            var grammar = new Grammar(new GrammarBuilder(commands));
+            recognizer.LoadGrammar(grammar);
+            recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isListening)
+                {
+                    // Stop recognition
+                    recognizer.RecognizeAsyncStop();
+                    isListening = false;
+                    button1.Text = "Start Listening";
+                }
+                else
+                {
+                    // Start recognition
+                    recognizer.SetInputToDefaultAudioDevice();
+                    recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                    isListening = true;
+                    button1.Text = "Stop Listening";
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An unexpected error occurred: " + ex.Message);
+            }
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Ensure UI updates are performed on the UI thread
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => Recognizer_SpeechRecognized(sender, e)));
+                return;
+            }
+
+            switch (e.Result.Text)
+            {
+                case "Scroll up":
+                    ScrollWindow(Keys.Up);
+                    break;
+                case "Scroll down":
+                    ScrollWindow(Keys.Down);
+                    break;
+                case "up audio":
+                    SetVolume(10);
+                    break;
+                case "down audio":
+                    SetVolume(-10);
+                    break;
+                case "mute audio":
+                    muteVolume();
+                    break;
+                case "close the app":
+                    Close();
+                    break;
+            }
+        }
+
+        public void SetVolume(int volume)
+        {
+            
+            // Get the default audio device
+            var device = new CoreAudioController().DefaultPlaybackDevice;
+
+            // Set the volume
+            device.Volume = GetVolume() + volume; // AudioSwitcher expects volume in percentage
+        }
+
+        public Double GetVolume()
+        {
+            // Get the default audio device
+            var device = new CoreAudioController().DefaultPlaybackDevice;
+
+            // Return the current volume as a range from 0.0 to 1.0
+            return device.Volume;
+        }
+
+        public void muteVolume()
+        {
+            // Get the default audio device
+            // Get the default audio device
+            var device = new CoreAudioController().DefaultPlaybackDevice;
+
+            // Set the volume
+            device.Volume = 0;
+        }
+
+        private void ScrollWindow(Keys key)
+        {
+            if (selectedWindowHandle != IntPtr.Zero)
+            {
+                if (SetForegroundWindow(selectedWindowHandle))
+                {
+                    byte vk = key == Keys.Up ? VK_UP : VK_DOWN;
+                    keybd_event(vk, 0, 0, 0); // Key down
+                    keybd_event(vk, 0, KEYEVENTF_KEYUP, 0); // Key up
+                }
+                else
+                {
+                    ShowWindowNotRunningMessage();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Choose an app from the combo box.");
+            }
+        }
+
+        private void ShowWindowNotRunningMessage()
+        {
+            var selectedItem = comboBoxWindows.SelectedItem as WindowItem;
+            if (selectedItem != null)
+            {
+                MessageBox.Show($"{selectedItem.Title} is not running.");
+            }
+        }
+
+        private void RefreshWindowList()
+        {
+            comboBoxWindows.Items.Clear();
+            EnumWindows((hWnd, lParam) =>
+            {
+                if (IsWindowVisible(hWnd))
+                {
+                    var windowText = new StringBuilder(256);
+                    GetWindowText(hWnd, windowText, windowText.Capacity);
+                    if (windowText.Length > 0)
+                    {
+                        comboBoxWindows.Items.Add(new WindowItem { Handle = hWnd, Title = windowText.ToString() });
+                    }
+                }
+                return true;
+            }, IntPtr.Zero);
+        }
+
+        private void comboBoxWindows_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxWindows.SelectedItem is WindowItem selectedItem)
+            {
+                selectedWindowHandle = selectedItem.Handle;
+            }
+        }
+
+        private void buttonScrollUp_Click(object sender, EventArgs e)
+        {
+            ScrollWindow(Keys.Up);
+        }
+
+        private void buttonScrollDown_Click(object sender, EventArgs e)
+        {
+            ScrollWindow(Keys.Down);
+        }
+
+        private void comboBoxWindows_Click(object sender, EventArgs e)
+        {
+            RefreshWindowList();
+        }
+
+        private class WindowItem
+        {
+            public IntPtr Handle { get; set; }
+            public string Title { get; set; }
+
+            public override string ToString() => Title;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (recognizer != null)
+            {
+                if (isListening)
+                {
+                    recognizer.RecognizeAsyncStop();
+                }
+                recognizer.Dispose();
+            }
+            base.OnFormClosing(e);
+        }
+    }
+
+
+}
